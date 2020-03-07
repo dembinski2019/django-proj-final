184091b93016cd2199174fb9c584a928d1ef0907
dembinski2019 <everton_s.d@hotmail.com>
2020-03-06 23:44:13 -0300

    teste

--- epark/urls.py
+++ epark/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import  include, path
-
+from core.views import home
 
 urlpatterns = [
     path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('',home, name='home')
 
 ]
