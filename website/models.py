bf26e688cd295627ea7cb7a1e982782086428f4a
dembinski2019 <everton_s.d@hotmail.com>
2020-03-25 16:51:39 -0300

    Criação de Templates

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Contato(models.Model):
+
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    email = models.EmailField()
+    noticias = models.BooleanField()
+    mensagem = models.TextField()
+
+    def __str__(self):
+        return self.nome
+    
